e079f4f1afb02d268b29ccc86cc2bee0188c1324
Maksym Stepanenko <stepanenkomaksi@gmail.com>
2025-12-28 13:02:11 +0200

    Wiremock test fixed

--- src/test/java/com/github/mstepan/demo_ai/service/ChatServiceWireMockTest.java
+++ src/test/java/com/github/mstepan/demo_ai/service/ChatServiceWireMockTest.java
@@ -1,90 +1,68 @@
 package com.github.mstepan.demo_ai.service;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.mstepan.demo_ai.web.Question;
 import com.github.tomakehurst.wiremock.client.ResponseDefinitionBuilder;
 import com.github.tomakehurst.wiremock.client.WireMock;
+import com.github.tomakehurst.wiremock.stubbing.Scenario;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
-import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.evaluation.EvaluationResponse;
-import org.springframework.ai.evaluation.Evaluator;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
 import org.springframework.core.io.Resource;
 import org.wiremock.spring.ConfigureWireMock;
 import org.wiremock.spring.EnableWireMock;
 
 import java.io.IOException;
-import java.nio.charset.Charset;
-import java.util.Map;
+import java.nio.charset.StandardCharsets;
 
 @EnableWireMock(@ConfigureWireMock(baseUrlProperties = "oci.genai.base.url"))
 @SpringBootTest(properties = "oci.genai.base.url=${oci.genai.base.url}")
 public class ChatServiceWireMockTest {
 
-    @Value("classpath:/test-oci-genai-response.json")
-    Resource responseResource;
+    @Value("classpath:/test-chat-response.json")
+    Resource chatResponse;
 
-    @Autowired ChatClient.Builder chatClientBuilder;
+    @Value("classpath:/test-relevance-evaluation-response.json")
+    Resource relevanceEvaluationResponse;
 
-    @Autowired
-    @Qualifier("ociGenAIRelevancyEvaluator")
-    Evaluator relevancyEvaluator;
-
-    @Value("classpath:/prompts/chat/chatSystemPrompt.st")
-    Resource systemPromptTemplate;
-
-    @Value("classpath:/prompts/chat/chatUserPrompt.st")
-    Resource userPromptTemplate;
-
-    @TestConfiguration
-    static class MockConfig {
-        @Bean
-        @Primary
-        @Qualifier("ociGenAIRelevancyEvaluator")
-        public Evaluator mockEvaluator() {
-            return Mockito.mock(Evaluator.class);
-        }
-    }
+    @Autowired ChatService chatService;
 
     @BeforeEach
     void setUp() throws IOException {
-        Mockito.reset(relevancyEvaluator);
-        var predefinedResponse = responseResource.getContentAsString(Charset.defaultCharset());
         var mapper = new ObjectMapper();
-        var responseNode = mapper.readTree(predefinedResponse);
+
+        var chatResponseNode =
+                mapper.readTree(chatResponse.getContentAsString(StandardCharsets.UTF_8));
+
+        var relevanceEvaluationResponseNode =
+                mapper.readTree(
+                        relevanceEvaluationResponse.getContentAsString(StandardCharsets.UTF_8));
+
         WireMock.stubFor(
                 WireMock.post("/20231130/actions/chat")
-                        .willReturn(ResponseDefinitionBuilder.okForJson(responseNode)));
+                        .inScenario("oci-chat-sequence")
+                        .whenScenarioStateIs(Scenario.STARTED)
+                        .willReturn(ResponseDefinitionBuilder.okForJson(chatResponseNode))
+                        .willSetStateTo("RELEVANCE_EVALUATION_CALL"));
+
+        WireMock.stubFor(
+                WireMock.post("/20231130/actions/chat")
+                        .inScenario("oci-chat-sequence")
+                        .whenScenarioStateIs("RELEVANCE_EVALUATION_CALL")
+                        .willReturn(
+                                ResponseDefinitionBuilder.okForJson(
+                                        relevanceEvaluationResponseNode)));
     }
 
     @Test
     void askQuestion() {
-        when(relevancyEvaluator.evaluate(any()))
-                .thenReturn(new EvaluationResponse(true, 1.0F, "", Map.of()));
-
-        var chatService =
-                new ChatService(
-                        chatClientBuilder,
-                        relevancyEvaluator,
-                        systemPromptTemplate,
-                        userPromptTemplate);
         var answer = chatService.askQuestion(new Question("What is the capital of France?"));
         assertThat(answer).isNotNull();
         assertThat(answer.answer()).isEqualTo("The capital of France is Paris.");
-
-        verify(relevancyEvaluator, times(1)).evaluate(any());
     }
 }
